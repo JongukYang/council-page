8720a7afe1a6008019fcda3411658fcab29eb335
jaminleee <jamin_00@naver.com>
2022-09-25 03:48:23 +0900

    make cards

--- council-page/src/components/Cards.js
+++ council-page/src/components/Cards.js
@@ -1,11 +1,19 @@
 import React from 'react'
 import CardItem from './CardItem';
 import './Cards.css';
+import Cardimg from './../images/start_img.png'
 
 function Card() {
   return (
     <div className='cards'>
       <p>SNS Card News</p>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem src = {Cardimg} path='https://github.com/HSU-Council-FE/council-page' />
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }

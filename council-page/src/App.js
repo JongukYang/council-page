593333dba1a1a7f50eac87d620d8c4984a1457df
5lft <88468423+5lft@users.noreply.github.com>
2022-09-25 11:11:29 +0900

    fixed untracked files

--- council-page/src/App.js
+++ council-page/src/App.js
@@ -1,26 +1,26 @@
-import logo from './logo.svg';
-import Navbar from './components/Navbar';
-import Footer from './components/Footer';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from "./logo.svg";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import "bootstrap/dist/css/bootstrap.min.css";
 // import {BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import { BrowserRouter as Router, Routes, Route} 
-from 'react-router-dom';
-import './App.css';
-import Main from './components/pages/Main';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Main from "./components/pages/Main";
+import Signin from "./components/pages/Signin";
 
 function App() {
-  return (
-    <>
-    <Router>
-      <Navbar/>
-      <Routes>
-      <Route path="/" element={<Main />} />
-      </Routes>
-    </Router>
-    </>
-      
-    
-  );
+    return (
+        <>
+            <Router>
+                <Navbar />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    <Route path="/sign-in" element={<Signin />} />
+                </Routes>
+                <Footer />
+            </Router>
+        </>
+    );
 }
 
 export default App;
--- council-page/src/components/pages/Main.js
+++ council-page/src/components/pages/Main.js
@@ -1,20 +1,18 @@
-import React from 'react'
-import '../../App.css';
-import Banner from '../Banner'
-import Footer from '../Footer'
-import Summary_pages from './../Summary_pages';
-import HSU_pages from './../HSU_pages';
-import Cards from '../Cards';
+import React from "react";
+import "../../App.css";
+import Banner from "../Banner";
+import Footer from "../Footer";
+import Summary_pages from "./../Summary_pages";
+import HSU_pages from "./../HSU_pages";
+import Cards from "../Cards";
 
-
-function Main(){
-    return(
+function Main() {
+    return (
         <>
-        <Banner/>
-        <Summary_pages/>
-        <HSU_pages/>
-        <Cards/>
-        <Footer />
+            <Banner />
+            <Summary_pages />
+            <HSU_pages />
+            <Cards />
         </>
     );
 }

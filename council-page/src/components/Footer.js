3d2aaa7baab2a8cb9b47273fabee3484b09ccde7
jaminleee <jamin_00@naver.com>
2022-09-26 03:45:15 +0900

    I'm hungry..Edit Footer

--- council-page/src/components/Footer.js
+++ council-page/src/components/Footer.js
@@ -7,12 +7,12 @@ function Footer(){
     return(
         <div className='footer-container'>
             <section className='footer-subscription'>
-                <p className='footer-subscription-text'>
+                <div className='footer-subscription-text'>
                     한성대학교 총학생회<br/>
                     서울특별시 성북구 삼선교로 16길 116 (진리관 206호)<br/>
                     02)-760-4182<br/>
                     Copyright © hansung_student. All rights reserved.
-                </p>
+                </div>
             </section>
         </div>
     )

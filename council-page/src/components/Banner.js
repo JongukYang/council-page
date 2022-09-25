44a27d8cc5d44f12d6a28b438f75434940f63d10
5lft <88468423+5lft@users.noreply.github.com>
2022-09-25 11:47:21 +0900

    commit

--- council-page/src/components/Banner.js
+++ council-page/src/components/Banner.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import '../App.css';
-import { Button } from './Button'
-import './Banner.css';
-import Bannerimg from './../images/Banner_img.png'
+import React from "react";
+import "../App.css";
+import { Button } from "./Button";
+import "./Banner.css";
+import Bannerimg from "./../images/Banner_img.png";
 
 function Banner() {
-  return (
-    <div className='Banner-container'>
-      <img src={Bannerimg} />
-    </div>
-  )
+    return (
+        <div className="Banner-container">
+            <img src={Bannerimg} />
+        </div>
+    );
 }
 
-export default Banner
+export default Banner;
--- council-page/src/components/pages/Main.js
+++ council-page/src/components/pages/Main.js
@@ -1,7 +1,6 @@
 import React from "react";
 import "../../App.css";
 import Banner from "../Banner";
-import Footer from "../Footer";
 import Summary_pages from "./../Summary_pages";
 import HSU_pages from "./../HSU_pages";
 import Cards from "../Cards";

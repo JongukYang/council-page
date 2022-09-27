bbec6eeef3aef77b9ecd8e88bfb4259b051c45e0
jaminleee <jamin_00@naver.com>
2022-09-28 02:22:52 +0900

    edit main notice area

--- council-page/src/components/pages/Main.js
+++ council-page/src/components/pages/Main.js
@@ -4,13 +4,14 @@ import Banner from "../Banner";
 import Summary_pages from "./../Summary_pages";
 import HSU_pages from "./../HSU_pages";
 import Cards from "../Cards";
-import Notice from './Notice/Notice';
+// import Notice from './Notice/Notice';
+import Notice_main from "./Main/Notice_main";
 
 function Main() {
     return (
         <>
             <Banner />
-            <Notice />
+            <Notice_main />
             <HSU_pages />
             <Cards />
         </>

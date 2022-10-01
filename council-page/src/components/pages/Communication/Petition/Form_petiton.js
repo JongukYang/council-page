22175cc264e9a185832e712843a9edb1f2e5c88a
jaminleee <jamin_00@naver.com>
2022-10-01 22:56:16 +0900

    petition form 약간 꾸밈

--- council-page/src/components/pages/Communication/Petition/Form_petiton.js
+++ council-page/src/components/pages/Communication/Petition/Form_petiton.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
+import './Form_petition.css'
 
 function Form_petiton() {
   return (
-    <Form>
+    <Form id='form_petition'>
       <Form.Group className="mb-3" controlId="formBasicEmail">
         <Form.Label>제목</Form.Label>
         <Form.Control type="email" placeholder="제목을 입력해주세요" />
@@ -17,7 +18,7 @@ function Form_petiton() {
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="비밀글 설정" />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button className='submit_button' variant="primary" type="submit">
         Submit
       </Button>
     </Form>
